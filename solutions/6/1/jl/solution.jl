9979add6d3c06e6c322ea78bd2e69afe17f6bb2b
Bas van Berckel <bas.vanberckel@sendcloud.com>
2022-12-06 09:05:02 +0100

    day 6 (jl) complete

---
+++ solutions/6/1/jl/solution.jl
@@ -0,0 +1,10 @@
+open("cases/input/1","r") do f
+    for input in eachline(f)
+        for i in 14:length(input)
+            if length(Set(input[i-13:i])) == 14
+                println(i)
+                break
+            end
+        end
+    end
+end
